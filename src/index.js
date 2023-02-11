12f6229f72471e395dfc7365c8b06ea5788e01a0
hdlee0619 <dlgusehd1211@naver.com>
2023-02-11 14:45:20 +0900

    Docs: Redux폴더 및 파일 추가

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import store from './redux/config/configStore';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
